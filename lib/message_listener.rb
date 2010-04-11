4e223131eb57180198a62acebfae484c27c09f29
Dan Tenenbaum <dante@viper.local>
2010-04-10 19:39:51 -0700

    using aws keys from config file

--- lib/message_listener.rb
+++ lib/message_listener.rb
@@ -7,7 +7,7 @@ class MessageListener
   require 'right_aws'
 
   #todo get this info from a secret place, don't hardcode it
-  sqs = RightAws::SqsGen2.new("xx","yy")
+  sqs = RightAws::SqsGen2.new(AWS_ACCOUNT_KEY,AWS_SECRET_KEY)
   queue1 = sqs.queue("cspotrun-from-instances")
   
   message = queue1.receive
