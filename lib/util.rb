faeccdf0bd9fff74bc5fb1d0c6f40acb1596c974
Dan Tenenbaum <dtenenbaum@systemsbiology.org>
2010-05-13 16:11:54 -0700

    tracking public ip of instances

--- lib/util.rb
+++ lib/util.rb
@@ -268,7 +268,7 @@ module Util
     for item in instance_request_results
       if (item.has_key?(:instance_id) and (f = instance_results.detect{|i|i[:aws_instance_id] == item[:instance_id]}))
         #lputs "we are here!"
-        cspotrun_instance = instances.detect{|i|i.sir_id == item[:instance_id]}
+        cspotrun_instance = instances.detect{|i|i.sir_id == f[:instance_id]}
         item[:cspotrun_instance_id] = cspotrun_instance.id unless cspotrun_instance.nil?
         item[:instance_info] = f
       end
